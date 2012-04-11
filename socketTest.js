49c22fbe834e973ede32f5529758992cac75e595
ide user ide_braindamageinc <braindamageinc@gmail.com>
2012-04-11 09:08:34 +0000

    tst

--- socketTest.js
+++ socketTest.js
@@ -1,3 +1,5 @@
+var http  = require('http');
+
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
@@ -18,22 +20,54 @@ function handler (req, res) {
 }
 
 var mIdx = 0;
+var clients = new Array();
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
+function onEventFromClient(data)
+{
     console.log(data);
-  }); 
+    
+    console.log(data.my);
+}
+
+function onSocketConnected(socket)
+{
+  socket.emit('news', { hello: 'hello world' });
+  
+  socket.on('my other event', onEventFromClient); 
+  
+  clients[mIdx] = socket.id;
+  mIdx++;
   
   var refresh = setInterval( function() {
-                var msg = "sugi " + mIdx + socket.id;
-                socket.emit('news', {hello: msg});
-                mIdx++;
+                for (var i=0; i < mIdx; i++)
+                {
+                    var msg = "<br>clients: " +  clients[i];
+                    socket.emit('news', {hello: msg});
+                }
              }, 2000 );
 
     socket.on('disconnect', function() {
         clearInterval(refresh);
     });
     
-});
+    //getGoogle();
+    
+}
+    
 
+io.sockets.on('connection', onSocketConnected);
+
+function getGoogle() {
+    var google = http.createClient(80, 'www.google.com');
+    var request = google.request('GET', '/',
+      {'host': 'www.google.com'});
+    request.end();
+    request.on('response', function (response) {
+      console.log('STATUS: ' + response.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(response.headers));
+      response.setEncoding('utf8');
+      response.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+      });
+    });
+}
